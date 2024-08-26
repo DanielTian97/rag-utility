bbc2609a81c2b7ee5b92abd82eda70e350010ca6
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-08-26 21:42:55 +0100

    debug script

--- script_2shot_10_10_20.sh
+++ script_2shot_10_10_20.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python rag-random_kshot.py 2 2 5 10 10 0.3 20
 echo 'Finished generation for 2shot for trec-dl-20!'
-python evaluation_result_full_20.py 2 5 20
+python evaluation_result_full.py 2 5 20
 echo 'Finished bert-score evaluation for 2shot for trec-dl-20!'
 
