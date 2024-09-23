7af4eeb84f5faf783d526ea9c5fd7a0d202ee3d0
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-23 19:16:19 +0100

    more experiments on 20

---
+++ script_3-5shot_1_0_20.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+python rag-random_kshot.py 3 3 5 1 0 0.3 20
+echo 'Finished generation for 3shot for trec-dl-20!'
+python evaluation_result_full.py 3 5 20
+echo 'Finished bert-score evaluation for 3shot for trec-dl-20!'
+python rag-random_kshot.py 4 4 5 1 0 0.3 20
+echo 'Finished generation for 4shot for trec-dl-20!'
+python evaluation_result_full.py 4 5 20
+echo 'Finished bert-score evaluation for 4shot for trec-dl-20!'
+python rag-random_kshot.py 5 5 5 1 0 0.3 20
+echo 'Finished generation for 5shot for trec-dl-20!'
+python evaluation_result_full.py 5 5 20
+echo 'Finished bert-score evaluation for 5shot for trec-dl-20!'
+
+
