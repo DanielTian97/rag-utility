804728f01663c3925b16790dd3c7626434ea1c30
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-25 19:15:15 +0100

    debug-8 prompt improvements

---
+++ script_prompt1_19_full_experiments_6to8.sh
@@ -0,0 +1,6 @@
+python rag-random_kshot_prompt1.py 6 1 5 5 0 0.3 19
+echo 'Finished generation for 6shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 7 1 5 4 0 0.3 19
+echo 'Finished generation for 7shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 8 1 5 3 0 0.3 19
+echo 'Finished generation for 8shot(new prompt) for trec-dl-19!'
