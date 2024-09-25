e202d27395165de86dea92ab331b1f89dabfcc2b
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-25 15:13:19 +0100

    experiments with new prompt

--- script_prompt1_19.sh
+++ script_prompt1_19.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-python rag-random_kshot.py 1 1 5 10 10 0.3 20
-echo 'Finished generation for 1shot for trec-dl-20!'
-python evaluation_result_full.py 1 5 20
-echo 'Finished bert-score evaluation for 1shot for trec-dl-20!'
+python rag-random_kshot_prompt1.py 1 1 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 2 2 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 3 3 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 4 4 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 5 5 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 6 6 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 7 7 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 8 8 5 1 0 0.3 19
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
