94318f92bb5307901ce3dd97aecfe05303e34b1f
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-28 21:26:29 +0100

    experiment script for 20-4pmts

---
+++ script_prompt1_20_full_experiments_2to5_p.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python rag-permutation-random_kshot_prompt1.py 2 1 5 1 0 0.3 False 20
+echo 'Finished generation for 2shot(new prompt) 4 permutations for trec-dl-20!'
+python rag-permutation-random_kshot_prompt1.py 3 1 5 1 0 0.3 False 20
+echo 'Finished generation for 3shot(new prompt) 4 permutations for trec-dl-20!'
+python rag-permutation-random_kshot_prompt1.py 4 1 5 1 0 0.3 False 20
+echo 'Finished generation for 4shot(new prompt) 4 permutations for trec-dl-20!'
+python rag-permutation-random_kshot_prompt1.py 5 1 5 1 0 0.3 False 20
+echo 'Finished generation for 5shot(new prompt) 4 permutations for trec-dl-20!'
