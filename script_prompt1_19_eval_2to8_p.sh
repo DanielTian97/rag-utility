b72c43fb0b28931de0954add1a2a58cdaa32044d
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-29 18:16:20 +0100

    eval 4pmts-19 debug

--- script_prompt1_19_eval_2to8_p.sh
+++ script_prompt1_19_eval_2to8_p.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-python evaluation_result_full_new_prompt.py 2 5 19 9 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 2 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 2shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 3 5 19 8 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 3 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 3shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 4 5 19 7 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 4 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 4shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 5 5 19 6 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 5 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 5shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 6 5 19 5 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 6 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 6shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 7 5 19 4 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 7 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 7shot for trec-dl-19(new prompt) with pmts!'
-python evaluation_result_full_new_prompt.py 8 5 19 3 0 _p_prompt1
+python evaluation_result_full_new_prompt.py 8 5 19 1 0 _p_prompt1
 echo 'Finished bert-score evaluation for 8shot for trec-dl-19(new prompt) with pmts!'
