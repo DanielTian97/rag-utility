3e4f3ff0268780a9fa35f9f2c4580b68f53b138b
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-09-30 17:22:47 +0100

    update evaluate function - create for 20 - 0930

---
+++ script_prompt1_20_eval_2to8_p.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+python evaluation_result_full_new_prompt.py 2 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 2shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 3 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 3shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 4 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 4shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 5 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 5shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 6 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 6shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 7 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 7shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 8 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 8shot for trec-dl-20(new prompt) with pmts!'
