11fcbe2252512c7b42bf72a0cc591760a4493481
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-10-02 23:33:59 +0100

    debug redo oracle experiments -_-

--- rag-random_kshot_prompt1.py
+++ rag-random_kshot_prompt1.py
@@ -28,7 +28,7 @@ if __name__=="__main__":
       # load the llm
       llm = llama_tools.load_llama()
       # load needed data
-      doc_dict, queries, res = prepare_data(dataset_name)
+      doc_dict, queries, res = prepare_data(dataset_name, retriever_name)
       
       setting_file_name = f'./middle_products/random_answers_{batch_size}shot_{num_calls}calls_{top_starts}_{tail_starts}_{retriever_name}_dl_{dataset_name}_prompt1_settings.json'
       setting_record = {'batch_size':batch_size, 'batch_step':batch_step, 'num_calls':num_calls, \
