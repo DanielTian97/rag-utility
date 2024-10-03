373f55b4eb2caba08a8bb22791fe5f962929677a
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-10-03 16:24:15 +0100

    eval for monot5 tests

---
+++ script_prompt1_20_mt5_eval_1to8.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+python evaluation_result_full_new_prompt.py 1 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 1shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 2 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 2shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 3 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 3shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 4 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 4shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 5 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 5shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 6 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 6shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 7 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 7shot for trec-dl-20(new prompt) mt5!'
+python evaluation_result_full_new_prompt.py 8 5 20 1 0 _prompt1 mt5
+echo 'Finished bert-score evaluation for 8shot for trec-dl-20(new prompt) mt5!'
