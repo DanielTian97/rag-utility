615a87fead2e4ab42421125d104a25de831a4a0a
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-10-04 22:38:10 +0100

    p test 9 to 12

---
+++ script_prompt1_19_full_experiments_9to12_p.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python rag-permutation-random_kshot_prompt1.py 9 1 5 1 0 0.3 False 19
+echo 'Finished generation for 6shot(new prompt) 4 permutations for trec-dl-19!'
+python rag-permutation-random_kshot_prompt1.py 10 1 5 1 0 0.3 False 19
+echo 'Finished generation for 7shot(new prompt) 4 permutations for trec-dl-19!'
+python rag-permutation-random_kshot_prompt1.py 11 1 5 1 0 0.3 False 19
+echo 'Finished generation for 8shot(new prompt) 4 permutations for trec-dl-19!'
+python rag-permutation-random_kshot_prompt1.py 12 1 5 1 0 0.3 False 19
+echo 'Finished generation for 8shot(new prompt) 4 permutations for trec-dl-19!'
