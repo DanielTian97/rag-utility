ed995551abc0efe421cf1bc22b7a9504382f88cd
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2024-10-07 01:17:13 +0100

    evals for 1-12 p

---
+++ script_prompt1_19_eval_9to12_p.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python evaluation_result_full_new_prompt.py 9 5 19 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 9shot for trec-dl-19(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 10 5 19 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 10shot for trec-dl-19(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 11 5 19 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 11shot for trec-dl-19(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 12 5 19 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 12shot for trec-dl-19(new prompt) with pmts!'
