4726ba27cdef40e58c97c25017ed0a2dc681b38a
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2024-10-07 01:51:52 +0100

    bm25 p-test on 13-16

---
+++ script_prompt1_20_eval_13to16_p.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python evaluation_result_full_new_prompt.py 13 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 13shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 14 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 14shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 15 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 15shot for trec-dl-20(new prompt) with pmts!'
+python evaluation_result_full_new_prompt.py 16 5 20 1 0 _p_prompt1
+echo 'Finished bert-score evaluation for 16shot for trec-dl-20(new prompt) with pmts!'
