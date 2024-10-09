eb29d31cd6d9454e50cb7757820723236fd3f974
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-10-09 11:11:15 +0100

    eval 13-16 p tests for monot5

---
+++ script_prompt1_20_mt5_eval_13to16_p.sh
@@ -0,0 +1,8 @@
+python evaluation_result_full_new_prompt.py 13 5 20 1 0 _p_prompt1 mt5
+echo 'Finished bert-score evaluation for 13shot for trec-dl-20(new prompt) mt5 with pmts!'
+python evaluation_result_full_new_prompt.py 14 5 20 1 0 _p_prompt1 mt5
+echo 'Finished bert-score evaluation for 14shot for trec-dl-20(new prompt) mt5 with pmts!'
+python evaluation_result_full_new_prompt.py 15 5 20 1 0 _p_prompt1 mt5
+echo 'Finished bert-score evaluation for 15shot for trec-dl-20(new prompt) mt5 with pmts!'
+python evaluation_result_full_new_prompt.py 16 5 20 1 0 _p_prompt1 mt5
+echo 'Finished bert-score evaluation for 16shot for trec-dl-20(new prompt) mt5 with pmts!'
