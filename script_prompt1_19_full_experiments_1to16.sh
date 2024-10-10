84770dc3bc4cf264007698a0661455fdf5fa4130
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-10-10 12:07:58 +0100

    add 1-16 bm25 test 5 0

---
+++ script_prompt1_19_full_experiments_1to16.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+python rag-random_kshot_prompt1.py 1 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 1shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 1 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 1shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 2 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 2shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 2 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 2shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 3 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 3shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 3 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 3shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 4 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 4shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 4 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 4shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 5 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 5shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 5 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 5shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 6 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 6shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 6 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 6shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 7 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 7shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 7 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 7shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 8 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 8shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 8 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 8shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 9 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 9shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 9 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 9shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 10 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 10shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 10 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 10shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 11 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 11shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 11 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 11shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 12 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 12shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 12 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 12shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 13 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 13shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 13 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 13shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 14 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 14shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 14 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 14shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 15 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 15shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 15 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 15shot(new prompt) for trec-dl-20!'
+python rag-random_kshot_prompt1.py 16 1 5 5 0 0.3 19 bm25
+echo 'Finished generation for 16shot(new prompt) for trec-dl-19!'
+python rag-random_kshot_prompt1.py 16 1 5 5 0 0.3 20 bm25
+echo 'Finished generation for 16shot(new prompt) for trec-dl-20!'
