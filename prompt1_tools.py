5e41ecfb06639b0b0b6b00b7ce606c7abc9a1aad
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2025-01-07 18:26:18 +0000

    debug multithread - the key type problem(5)

--- prompt1_tools.py
+++ prompt1_tools.py
@@ -46,7 +46,7 @@ def compose_context(res, qid: str, batch_size, batch_step, top_starts, tail_star
     print(qid)
     retrieved_for_q = res[res.qid==qid]
     retrieved_num = retrieved_for_q['rank'].max()+1
-    print(type(batch_size), type(batch_step))
+    print(type(retrieved_num))
     starts = list(range(0, (retrieved_num-1)-(batch_size-1)+1, batch_step))
     start_rank_list = list(set(starts[:top_starts]).union(set(starts[(len(starts)-1)-(tail_starts-1):])))
     start_rank_list.sort()
