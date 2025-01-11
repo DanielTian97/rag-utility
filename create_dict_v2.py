abc1b2c7f40483694329beb2d7d6182fc680f1bb
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2025-01-11 23:29:53 +0000

    debug support v2 -- (4)

--- create_dict_v2.py
+++ create_dict_v2.py
@@ -30,5 +30,5 @@ for i in dataset.get_corpus_iter(verbose=True):
     if(i['docno'] in docnos):
         doc_dict.update({i['docno']: i['text']})
 
-with open('./middle_products/msmarco_passage_v2_dict.pkl', 'wb') as f:
+with open('./middle_products/msmarco_passage_dict_v2.pkl', 'wb') as f:
     pickle.dump(doc_dict, f)
