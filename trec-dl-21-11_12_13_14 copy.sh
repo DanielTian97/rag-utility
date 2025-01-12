7c5429d8f6b4e9c25cc64fa21db9e743c1b0c271
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2025-01-12 20:04:56 +0000

    21 22 tests for bm25 3 4 6 7

---
+++ trec-dl-21-11_12_13_14 copy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+python rag-random_kshot_prompt1.py 3 1 5 1 0 0.3 21 bm25
+echo 'Finished generation for 3shot for trec-dl-21!'
+python evaluation_result_full_new_prompt.py 3 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 3shot for trec-dl-20!'
+python rag-random_kshot_prompt1.py 3 1 5 1 0 0.3 22 bm25
+echo 'Finished generation for 3shot for trec-dl-22!'
+python evaluation_result_full_new_prompt.py 3 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 3shot for trec-dl-20!'
+
+python rag-random_kshot_prompt1.py 4 1 5 1 0 0.3 21 bm25
+echo 'Finished generation for 4shot for trec-dl-21!'
+python evaluation_result_full_new_prompt.py 4 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 4shot for trec-dl-20!'
+python rag-random_kshot_prompt1.py 4 1 5 1 0 0.3 22 bm25
+echo 'Finished generation for 4shot for trec-dl-22!'
+python evaluation_result_full_new_prompt.py 4 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 4shot for trec-dl-20!'
+
+python rag-random_kshot_prompt1.py 6 1 5 1 0 0.3 21 bm25
+echo 'Finished generation for 6shot for trec-dl-21!'
+python evaluation_result_full_new_prompt.py 6 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 6shot for trec-dl-20!'
+python rag-random_kshot_prompt1.py 6 1 5 1 0 0.3 22 bm25
+echo 'Finished generation for 6shot for trec-dl-22!'
+python evaluation_result_full_new_prompt.py 6 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 6shot for trec-dl-20!'
+
+python rag-random_kshot_prompt1.py 7 1 5 1 0 0.3 21 bm25
+echo 'Finished generation for 7shot for trec-dl-21!'
+python evaluation_result_full_new_prompt.py 7 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 7shot for trec-dl-20!'
+python rag-random_kshot_prompt1.py 7 1 5 1 0 0.3 22 bm25
+echo 'Finished generation for 7shot for trec-dl-22!'
+python evaluation_result_full_new_prompt.py 7 5 dev_small 1 0 _prompt1 bm25
+echo 'Finished bert-score evaluation for 7shot for trec-dl-20!'
+
+
+
