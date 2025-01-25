ba47cd511f7d872e7f2a376251509ab4eb083b9d
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2025-01-25 02:01:12 +0000

    full tests for tct 19 and 20

--- tct_tests_19.sh
+++ tct_tests_19.sh
@@ -1,5 +1,20 @@
+python rag-random_kshot.py --k 16 --dataset_name 19 --retriever tct
+python evaluator.py --k 16 --dataset_name 19 --retriever tct
+
 python rag-random_kshot.py --k 15 --dataset_name 19 --retriever tct
 python evaluator.py --k 15 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 14 --dataset_name 19 --retriever tct
+python evaluator.py --k 14 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 13 --dataset_name 19 --retriever tct
+python evaluator.py --k 13 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 12 --dataset_name 19 --retriever tct
+python evaluator.py --k 12 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 11 --dataset_name 19 --retriever tct
+python evaluator.py --k 11 --dataset_name 19 --retriever tct
 
 python rag-random_kshot.py --k 10 --dataset_name 19 --retriever tct
 python evaluator.py --k 10 --dataset_name 19 --retriever tct
@@ -7,5 +22,14 @@ python evaluator.py --k 10 --dataset_name 19 --retriever tct
 python rag-random_kshot.py --k 5 --dataset_name 19 --retriever tct
 python evaluator.py --k 5 --dataset_name 19 --retriever tct
 
+python rag-random_kshot.py --k 4 --dataset_name 19 --retriever tct
+python evaluator.py --k 4 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 3 --dataset_name 19 --retriever tct
+python evaluator.py --k 3 --dataset_name 19 --retriever tct
+
 python rag-random_kshot.py --k 2 --dataset_name 19 --retriever tct
 python evaluator.py --k 2 --dataset_name 19 --retriever tct
+
+python rag-random_kshot.py --k 1 --dataset_name 19 --retriever tct
+python evaluator.py --k 1 --dataset_name 19 --retriever tct
