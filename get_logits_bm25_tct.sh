5eda6055924a706ddc6f668c6fd35c6e785b76ed
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2025-01-30 11:18:23 +0000

    debug

--- get_logits_bm25_tct.sh
+++ get_logits_bm25_tct.sh
@@ -1,19 +1,19 @@
-python rag-random_kshot_with_permutations.python --k 2 --dataset_name 19 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 5 --dataset_name 19 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 10 --dataset_name 19 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 15 --dataset_name 19 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 2 --dataset_name 19 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 5 --dataset_name 19 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 10 --dataset_name 19 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 15 --dataset_name 19 --retriever bm25
 
-python rag-random_kshot_with_permutations.python --k 2 --dataset_name 20 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 5 --dataset_name 20 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 10 --dataset_name 20 --retriever bm25
-python rag-random_kshot_with_permutations.python --k 15 --dataset_name 20 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 2 --dataset_name 20 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 5 --dataset_name 20 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 10 --dataset_name 20 --retriever bm25
+python rag-random_kshot_with_permutations.py --k 15 --dataset_name 20 --retriever bm25
 
-python rag-random_kshot.python --k 2 --dataset_name 19 --retriever tct
-python rag-random_kshot.python --k 5 --dataset_name 19 --retriever tct
-python rag-random_kshot.python --k 10 --dataset_name 19 --retriever tct
-python rag-random_kshot.python --k 15 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 2 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 5 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 10 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 15 --dataset_name 19 --retriever tct
 
-python rag-random_kshot.python --k 2 --dataset_name 20 --retriever tct
-python rag-random_kshot.python --k 5 --dataset_name 20 --retriever tct
-python rag-random_kshot.python --k 10 --dataset_name 20 --retriever tct
-python rag-random_kshot.python --k 15 --dataset_name 20 --retriever tct
+python rag-random_kshot.py --k 2 --dataset_name 20 --retriever tct
+python rag-random_kshot.py --k 5 --dataset_name 20 --retriever tct
+python rag-random_kshot.py --k 10 --dataset_name 20 --retriever tct
+python rag-random_kshot.py --k 15 --dataset_name 20 --retriever tct
