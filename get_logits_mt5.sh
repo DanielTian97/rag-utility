8192e9c223b791b194b9945b689e6fb02f7e7449
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2025-01-30 11:19:28 +0000

    debug-2

--- get_logits_mt5.sh
+++ get_logits_mt5.sh
@@ -1,4 +1,4 @@
-python rag-random_kshot_with_permutations.pyt --k 2 --dataset_name 19 --retriever mt5
+python rag-random_kshot_with_permutations.py --k 2 --dataset_name 19 --retriever mt5
 python rag-random_kshot_with_permutations.py --k 5 --dataset_name 19 --retriever mt5
 python rag-random_kshot_with_permutations.py --k 10 --dataset_name 19 --retriever mt5
 python rag-random_kshot_with_permutations.py --k 15 --dataset_name 19 --retriever mt5
