9c15933692e6ed95a05ff7efbb35790cc0551d5b
FangzhengAndrewTian <2612785T@student.gla.ac.uk>
2025-02-05 04:29:24 +0000

    evaluated for 2 10 15

---
+++ evauation_2_10_15.sh
@@ -0,0 +1,39 @@
+python evaluator.py --k 2 --dataset_name 19 --retriever bm25 --suffix _p
+python evaluator.py --k 2 --dataset_name 19 --retriever bm25
+python evaluator.py --k 2 --dataset_name 20 --retriever bm25 --suffix _p
+python evaluator.py --k 2 --dataset_name 20 --retriever bm25
+python evaluator.py --k 2 --dataset_name 19 --retriever mt5 --suffix _p
+python evaluator.py --k 2 --dataset_name 19 --retriever mt5
+python evaluator.py --k 2 --dataset_name 20 --retriever mt5 --suffix _p
+python evaluator.py --k 2 --dataset_name 20 --retriever mt5
+python evaluator.py --k 2 --dataset_name 19 --retriever tct --suffix _p
+python evaluator.py --k 2 --dataset_name 19 --retriever tct
+python evaluator.py --k 2 --dataset_name 20 --retriever tct --suffix _p
+python evaluator.py --k 2 --dataset_name 20 --retriever tct
+
+python evaluator.py --k 10 --dataset_name 19 --retriever bm25 --suffix _p
+python evaluator.py --k 10 --dataset_name 19 --retriever bm25
+python evaluator.py --k 10 --dataset_name 20 --retriever bm25 --suffix _p
+python evaluator.py --k 10 --dataset_name 20 --retriever bm25
+python evaluator.py --k 10 --dataset_name 19 --retriever mt5 --suffix _p
+python evaluator.py --k 10 --dataset_name 19 --retriever mt5
+python evaluator.py --k 10 --dataset_name 20 --retriever mt5 --suffix _p
+python evaluator.py --k 10 --dataset_name 20 --retriever mt5
+python evaluator.py --k 10 --dataset_name 19 --retriever tct --suffix _p
+python evaluator.py --k 10 --dataset_name 19 --retriever tct
+python evaluator.py --k 10 --dataset_name 20 --retriever tct --suffix _p
+python evaluator.py --k 10 --dataset_name 20 --retriever tct
+
+python evaluator.py --k 15 --dataset_name 19 --retriever bm25 --suffix _p
+python evaluator.py --k 15 --dataset_name 19 --retriever bm25
+python evaluator.py --k 15 --dataset_name 20 --retriever bm25 --suffix _p
+python evaluator.py --k 15 --dataset_name 20 --retriever bm25
+python evaluator.py --k 15 --dataset_name 19 --retriever mt5 --suffix _p
+python evaluator.py --k 15 --dataset_name 19 --retriever mt5
+python evaluator.py --k 15 --dataset_name 20 --retriever mt5 --suffix _p
+python evaluator.py --k 15 --dataset_name 20 --retriever mt5
+python evaluator.py --k 15 --dataset_name 19 --retriever tct --suffix _p
+python evaluator.py --k 15 --dataset_name 19 --retriever tct
+python evaluator.py --k 15 --dataset_name 20 --retriever tct --suffix _p
+python evaluator.py --k 15 --dataset_name 20 --retriever tct
+
