e633e4d3dd290ae0721bc7519cc9fb62d7f3cda3
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2025-02-05 22:25:39 +0000

    more exp on 11 12 13 14 16

---
+++ experiment_11_12_13_14_16.sh
@@ -0,0 +1,27 @@
+python rag-random_kshot.py --k 12 --dataset_name 19 --retriever bm25
+python rag-random_kshot.py --k 12 --dataset_name 20 --retriever bm25
+python rag-random_kshot.py --k 12 --dataset_name 19 --retriever mt5
+python rag-random_kshot.py --k 12 --dataset_name 20 --retriever mt5
+python rag-random_kshot.py --k 12 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 12 --dataset_name 20 --retriever tct
+
+python rag-random_kshot.py --k 11 --dataset_name 19 --retriever bm25
+python rag-random_kshot.py --k 11 --dataset_name 20 --retriever bm25
+python rag-random_kshot.py --k 11 --dataset_name 19 --retriever mt5
+python rag-random_kshot.py --k 11 --dataset_name 20 --retriever mt5
+python rag-random_kshot.py --k 11 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 11 --dataset_name 20 --retriever tct
+
+python rag-random_kshot.py --k 16 --dataset_name 19 --retriever bm25
+python rag-random_kshot.py --k 16 --dataset_name 20 --retriever bm25
+python rag-random_kshot.py --k 16 --dataset_name 19 --retriever mt5
+python rag-random_kshot.py --k 16 --dataset_name 20 --retriever mt5
+python rag-random_kshot.py --k 16 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 16 --dataset_name 20 --retriever tct
+
+python rag-random_kshot.py --k 13 --dataset_name 19 --retriever bm25
+python rag-random_kshot.py --k 13 --dataset_name 20 --retriever bm25
+python rag-random_kshot.py --k 13 --dataset_name 19 --retriever mt5
+python rag-random_kshot.py --k 13 --dataset_name 20 --retriever mt5
+python rag-random_kshot.py --k 13 --dataset_name 19 --retriever tct
+python rag-random_kshot.py --k 13 --dataset_name 20 --retriever tct
